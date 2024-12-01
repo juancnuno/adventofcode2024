4a459f4d88bd9cd0ae8a77fbf08226331fff44a3
Juan C Nuno <juancnuno@gmail.com>
2024-12-01 12:07:23 -0800

    Solve Day 1 Part 2

---
+++ src/main/java/com/juancnuno/adventofcode2024/day01/Part2.java
@@ -0,0 +1,36 @@
+package com.juancnuno.adventofcode2024.day01;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public final class Part2 {
+
+    private Part2() {
+    }
+
+    public static long getSimilarityScore(Input input) {
+        var pattern = Pattern.compile(" +");
+        var list1 = new ArrayList<Integer>();
+        var list2 = new ArrayList<Integer>();
+
+        try (var lines = input.lines()) {
+            lines.forEach(line -> {
+                var ids = pattern.split(line);
+
+                list1.add(Integer.valueOf(ids[0]));
+                list2.add(Integer.valueOf(ids[1]));
+            });
+        }
+
+        var map = list2.stream().collect(Collectors.groupingBy(id -> id, Collectors.counting()));
+
+        return list1.stream()
+                .mapToLong(id -> id * map.getOrDefault(id, 0L))
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Part2.getSimilarityScore(new PathInput()));
+    }
+}
---
+++ src/test/java/com/juancnuno/adventofcode2024/day01/test/Part2Test.java
@@ -0,0 +1,29 @@
+package com.juancnuno.adventofcode2024.day01.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.juancnuno.adventofcode2024.day01.Part2;
+import com.juancnuno.adventofcode2024.day01.StringInput;
+
+final class Part2Test {
+
+    @Test
+    void getSimilarityScore() {
+        // Arrange
+        var input = new StringInput("""
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+            """);
+
+        // Act
+        var score = Part2.getSimilarityScore(input);
+
+        // Assert
+        assertEquals(31, score);
+    }
+}
