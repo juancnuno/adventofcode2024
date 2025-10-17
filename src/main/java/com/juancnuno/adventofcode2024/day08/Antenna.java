d76287a980fe6ec2d4229000185f38680793ba8d
Juan C Nuno <juancnuno@gmail.com>
2025-10-16 19:42:54 -0700

    Add getFrequencyToAntennaMultimap

---
+++ src/main/java/com/juancnuno/adventofcode2024/day08/Antenna.java
@@ -0,0 +1,14 @@
+package com.juancnuno.adventofcode2024.day08;
+
+final class Antenna {
+
+    private final char frequency;
+    private final int x;
+    private final int y;
+
+    Antenna(char frequency, int x, int y) {
+        this.frequency = frequency;
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/com/juancnuno/adventofcode2024/day08/Map.java
+++ src/main/java/com/juancnuno/adventofcode2024/day08/Map.java
@@ -1,5 +1,9 @@
 package com.juancnuno.adventofcode2024.day08;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
 import com.juancnuno.adventofcode2024.Input;
 
 public final class Map {
@@ -12,5 +16,26 @@ public final class Map {
 
     public int getAntinodeCount() {
         return 0;
+    }
+
+    private java.util.Map<Character, Collection<Antenna>> getFrequencyToAntennaMultimap() {
+        var list = input.lineList();
+        var multimap = new HashMap<Character, Collection<Antenna>>();
+
+        for (int y = 0, yMax = list.size(); y < yMax; y++) {
+            var line = list.get(y);
+
+            for (int x = 0, xMax = line.length(); x < xMax; x++) {
+                var c = line.charAt(x);
+
+                if (c == '.') {
+                    continue;
+                }
+
+                multimap.computeIfAbsent(c, _ -> new ArrayList<>()).add(new Antenna(c, x, y));
+            }
+        }
+
+        return multimap;
     }
 }
